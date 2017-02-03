f9cacc1eeb2a787a354449bfd3c5fceca19dfa08
Angor <angor@inbox.ru>
2017-02-03 15:32:51 +0300

    очень правдоподобное поведение авто

--- core/src/ru/angorstv/robobuttle/entities/RoboFrame.java
+++ core/src/ru/angorstv/robobuttle/entities/RoboFrame.java
@@ -18,14 +18,17 @@ public class RoboFrame extends PhysicsEntity {
 	private Engine engine;
 	private Battery battery;
 
+	private final float maxSpeed = 3f;
+	private final float maxAngularVelocity = 0.7f;
+
 	private float direct;
 
 	private Vector2 target;
 
 	private Array<Module> modules = new Array<>();
 	public List<Wheel> wheels;
-	private float wheelAngle =1f;
-	private float power =1;
+	private float wheelAngle = 1f;
+	private float power = 1;
 
 	public RoboFrame(String spriteName, World world, float x, float y) {
 		createSprite(spriteName);
@@ -34,8 +37,8 @@ public class RoboFrame extends PhysicsEntity {
 
 		//initialize wheels
 		this.wheels = new ArrayList<Wheel>();
-		this.wheels.add(new Wheel(world, this, -0.2f, 0.15f, 0.07f, 0.15f, true,  true)); //top left
-		this.wheels.add(new Wheel(world, this, 0.2f, 0.15f, 0.07f, 0.15f, true,  true)); //top right
+		this.wheels.add(new Wheel(world, this, -0.2f, 0.15f, 0.07f, 0.15f, true, true)); //top left
+		this.wheels.add(new Wheel(world, this, 0.2f, 0.15f, 0.07f, 0.15f, true, true)); //top right
 		//this.wheels.add(new Wheel(world, this, -0.2f, -0.15f, 0.07f, 0.15f, false,  false)); //back left
 		//this.wheels.add(new Wheel(world, this, 0.2f, -0.15f, 0.07f, 0.15f, false, false)); //back right
 	}
@@ -44,18 +47,18 @@ public class RoboFrame extends PhysicsEntity {
 		this.target = target;
 	}
 
-	public List<Wheel> getPoweredWheels () {
+	public List<Wheel> getPoweredWheels() {
 		List<Wheel> poweredWheels = new ArrayList<Wheel>();
-		for (Wheel wheel:this.wheels) {
+		for (Wheel wheel : this.wheels) {
 			if (wheel.powered)
 				poweredWheels.add(wheel);
 		}
 		return poweredWheels;
 	}
 
-	public List<Wheel> getRevolvingWheels () {
+	public List<Wheel> getRevolvingWheels() {
 		List<Wheel> revolvingWheels = new ArrayList<Wheel>();
-		for (Wheel wheel:this.wheels) {
+		for (Wheel wheel : this.wheels) {
 			if (wheel.revolving)
 				revolvingWheels.add(wheel);
 		}
@@ -79,88 +82,97 @@ public class RoboFrame extends PhysicsEntity {
 
 	}
 
-	private void testWheels(){
+	private void testWheels() {
 		//1. KILL SIDEWAYS VELOCITY
-		for(Wheel wheel:wheels){
+		for (Wheel wheel : wheels) {
 			wheel.killSidewaysVelocity();
 		}
 
 		//update revolving wheels
-		for(Wheel wheel:this.getRevolvingWheels()) {
+		for (Wheel wheel : this.getRevolvingWheels()) {
 			wheel.setAngle(this.wheelAngle);
 		}
 
 		//3. APPLY FORCE TO WHEELS
-		Vector2 baseVector = new Vector2(0,0.1f); //vector pointing in the direction force will be applied to a wheel ; relative to the wheel.
+		Vector2 baseVector = new Vector2(0, 0.1f); //vector pointing in the direction force will be applied to a wheel ; relative to the wheel.
 
 		//multiply by engine power, which gives us a force vector relative to the wheel
-		Vector2 forceVector= new Vector2(this.power*baseVector.x, this.power*baseVector.y);
+		Vector2 forceVector = new Vector2(this.power * baseVector.x, this.power * baseVector.y);
 
 		//apply force to each wheel
-		for(Wheel wheel:this.getPoweredWheels()){
-			Vector2 position= wheel.body.getWorldCenter();
-			wheel.body.applyForce(wheel.body.getWorldVector(new Vector2(forceVector.x, forceVector.y)), position, true );
+		for (Wheel wheel : this.getPoweredWheels()) {
+			Vector2 position = wheel.body.getWorldCenter();
+			wheel.body.applyForce(wheel.body.getWorldVector(new Vector2(forceVector.x, forceVector.y)), position, true);
 		}
 	}
 
-	private void testMoveWORotation(){
-		direct = body.getAngle();
-		Vector2 dir = new Vector2(0,1);
+	private void testMoveWORotation() {
+
+		//локальное напрвление на прямо
+		Vector2 dir = new Vector2(0, 1);
 
 		//вектор на цель из центра масс
 		Vector2 toTarget = new Vector2(target);
 		toTarget.sub(body.getPosition());
-		Vector2 a = body.getLocalVector(toTarget);
+		//пересчитывает этот вектор для локальных координат тела
+		Vector2 localToTarget = body.getLocalVector(toTarget);
 
 		// угол между направлением body и направлением на цель
-		float angle = a.angle(dir);
+		// положительный по часовой стрелке от +Y
+		// и отрицательный против часовой от +Y
+		float angle = localToTarget.angle(dir);
 
-		if (angle >0){
-				body.applyTorque(0.05f, true);
+		if (angle == 0) {
+			body.setAngularVelocity(0);
+		} else {
+			if (angle > 0) {
+				rotate(-1);
 			} else {
-				body.applyTorque(-0.05f, true);
+				rotate(1);
 			}
-//		angle = degreesToRadians * angle;
-//		if (direct != angle) {
-//			if (direct < angle) {
-//				body.applyTorque(0.05f, true);
-//			} else {
-//				body.applyTorque(-0.05f, true);
-//			}
-//		}
+		}
 
 		// газ и тормоз в зависимости от растояни от точки назначения
 		if(target.dst(body.getPosition())>sprite.getHeight()/PIXELS_PER_METER){
-			body.applyForceToCenter(body.getWorldVector(new Vector2(0,0.1f)), true);
+			if (getSpeed() < maxSpeed){
+				body.applyForceToCenter(body.getWorldVector(new Vector2(0,0.3f)), true);
+			}
 		}else{
-			Vector2 v = body.getLinearVelocityFromLocalPoint(this.body.getPosition());
-			v.nor();
-			v.x = v.x *-0.3f;
-			v.y = v.y * -0.3f;
-			body.applyForceToCenter(v,true);
-		}
-
-	}
-
-	private void testForvardMoveAndRotate(){
-		Vector2 force = new Vector2(target);
-		force.sub(body.getPosition());
-		force.nor();
-		// угол между направлением body и направлением на цель в градусах
-		float angle = force.angle(body.getPosition());
-		direct = body.getAngle();
-		if (direct != angle) {
-			if (direct > angle) {
-				body.applyTorque(0.1f, true);
-			} else {
-				body.applyTorque(-0.1f, true);
+			if (getSpeed() > 0){
+				body.applyForceToCenter(body.getWorldVector(new Vector2(0,-0.6f)), true);
 			}
 		}
-		//приложение силы соосно телу
-		//body.applyForceToCenter(new Vector2(MathUtils.sin(direct), MathUtils.cos(direct)), true);
-
+		killSideAwaysVelocity();
 
 	}
 
+	/**
+	 * повернуть тело
+	 * @param dir -1 по часовой, 1 против
+	 */
+	private void rotate (int dir){
+		if (body.getAngularVelocity() < maxAngularVelocity){
+			body.applyTorque(0.1f * dir, true);
+		}
+	}
+
+	/**
+	 * получить скорость по направлению движения
+	 * под вопросом ???
+	 * @return
+	 */
+	private float getSpeed (){
+		return body.getLocalVector(body.getLinearVelocity()).y;
+	}
+
+	private float getSideAwaysVelocity () {
+		return body.getLocalVector(body.getLinearVelocity()).x;
+	}
+
+	private void killSideAwaysVelocity (){
+		Vector2 v = body.getLocalVector(body.getLinearVelocity());
+		v.x = 0;
+		body.setLinearVelocity(body.getWorldVector(v));
+	}
 
 }
